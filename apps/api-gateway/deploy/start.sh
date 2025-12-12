e5ab682d651f806bd6aedfee2fe2877bcbe24afb
root <prueba@gmail.com>
2025-12-12 16:40:23 +0100

    Deploy: ms-usuarios & api-gateway

--- apps/api-gateway/deploy/start.sh
+++ apps/api-gateway/deploy/start.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 set -e
 
-config_git(){
-    git clone https://github.com/Backend-ukiyo/ukiyo-backend.git
-}
-
-#....
-
 main(){
+
     npm install -g npm@11.7.0
     npm install -g pnpm
     npm install -g pm2
 
+    echo "Instalando dependencias"
     pnpm install --frozen-lockfile
+
+    echo "Arrancando Gateway con PM2"
     
+    # Limpieza preventiva
     pm2 delete api-gateway 2>/dev/null || true
+    
+    # Arrancamos el proceso
     pm2 start pnpm --name "api-gateway" -- start:dev:gateway
+
+    echo "API-GATEWAY ONLINE (Puerto 3000)"
+    
 }
+
 main
+
+# Mantener el contenedor vivo mostrando logs
+pm2 logs api-gateway
--- apps/ms-usuarios/deploy/start.sh
+++ apps/ms-usuarios/deploy/start.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 set -e
 
-config.git(){
-    git clone https://github.com/Backend-ukiyo/ukiyo-backend.git
+# Función para esperar a que la BD esté lista
+wait_for_db() {
+    echo "Esperando a que la Base de Datos ($DB_HOST:$DB_PORT) esté lista..."
+        until nc -z -v -w30 "$DB_HOST" 5432; do
+    echo "Esperando BD"
+        sleep 2
+        done
+    echo "Base de datos conectada."
 }
 
-#....
-
 main(){
+    echo "Iniciando configuración automática"
+
     npm install -g npm@11.7.0
     npm install -g pnpm
     npm install -g pm2
 
+    echo "Instalando dependencias"
+
     pnpm install --frozen-lockfile
 
-    echo "Data Base: $DB_NAME"
+    echo "Base de datos: $DB_NAME en $DB_HOST"
+
+    wait_for_db 
 
     cd apps/ms-usuarios
+    
     pnpm exec prisma generate
-
     pnpm exec prisma migrate deploy
     
     cd ../..
     
+    echo "Arrancando PM2.."
     pm2 delete ms-usuarios 2>/dev/null || true
-    
     pm2 start pnpm --name "ms-usuarios" -- start:dev:usuarios
-
+    
+    echo "MS-USUARIOS ESTÁ ONLINE"
 }
+
 main
-tail -f /dev/null
+
+# Mantener el contenedor vivo mostrando los logs
+pm2 logs ms-usuarios
