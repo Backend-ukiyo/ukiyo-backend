e871e5662a148f3b4ab4eec949e7656581a21a4e
root <prueba@gmail.com>
2025-12-12 17:42:12 +0100

    Deploy: ms-clientes

--- apps/ms-clientes/config/envs.ts
+++ apps/ms-clientes/config/envs.ts
@@ -1,31 +1,29 @@
 import * as joi from 'joi';
+
 if (process.env.NODE_ENV !== 'production') {
-
     const path = require('path');
-
     const dotenv = require('dotenv');
 
     const envFilePath = path.resolve(process.cwd(), 'apps/ms-clientes/.env');
     dotenv.config({ path: envFilePath });
 }
-
 interface EnvVars {
-    CLIENTES_TCP_PORT: number;
-    CLIENTES_DB_HOST: string;
-    CLIENTES_DB_PORT: number;
-    CLIENTES_DB_USER: string;
-    CLIENTES_DB_PASS: string;
-    CLIENTES_DB_NAME: string;
+    PORT: number;
+    DB_HOST: string;
+    DB_PORT: number;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
     DATABASE_URL: string;
 }
 
 const envsSchema = joi.object({
-    CLIENTES_TCP_PORT: joi.number().required(),
-    CLIENTES_DB_HOST: joi.string().required(),
-    CLIENTES_DB_PORT: joi.number().required(),
-    CLIENTES_DB_USER: joi.string().required(),
-    CLIENTES_DB_PASS: joi.string().required(),
-    CLIENTES_DB_NAME: joi.string().required(),
+    PORT: joi.number().required(),
+    DB_HOST: joi.string().required(),
+    DB_PORT: joi.number().required(),
+    DB_USERNAME: joi.string().required(),
+    DB_PASSWORD: joi.string().required(),
+    DB_NAME: joi.string().required(),
     DATABASE_URL: joi.string().required(),
 })
 .unknown(true);
@@ -39,13 +37,13 @@ if (error) {
 const envVars: EnvVars = value;
 
 export const envs = {
-    port: envVars.CLIENTES_TCP_PORT,
+    port: envVars.PORT, 
     databaseUrl: envVars.DATABASE_URL,
     db: {
-        host: envVars.CLIENTES_DB_HOST,
-        port: envVars.CLIENTES_DB_PORT,
-        username: envVars.CLIENTES_DB_USER,
-        password: envVars.CLIENTES_DB_PASS,
-        name: envVars.CLIENTES_DB_NAME,
+        host: envVars.DB_HOST,
+        port: envVars.DB_PORT,
+        username: envVars.DB_USERNAME,
+        password: envVars.DB_PASSWORD,
+        name: envVars.DB_NAME,
     },
 };
